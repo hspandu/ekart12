f4a8cd44e5879a470f188946bd9e7f0a28348157
Hari sankar T <harisankart40@gmail.com>
2018-02-22 12:45:00 +0530

    code modified

--- login.java
+++ login.java
@@ -2,7 +2,7 @@ public class string
 {
 	pubhlic static void main(string args[])
 	{
-		system.out.println("hello");
+		system.out.println("hello world");
 		system.out.println("Goodmorning");
 	}
 }
